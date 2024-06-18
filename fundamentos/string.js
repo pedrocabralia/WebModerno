bda4850747d1e45011589b07c3c76ecba284c43b
pedrocabralia <pedrocabralia@hotmail.com>
2024-06-18 12:31:18 -0300

    fundamentos

---
+++ fundamentos/string.js
@@ -0,0 +1,12 @@
+let string = "teste";
+let string2 = 'teste'
+let string3 = `teste`;
+
+
+console.log(typeof string)
+console.log(typeof string2)
+console.log(typeof string3)
+
+
+console.log(string.charAt(1)) // retorna o caracter inicia no 0 
+console.log(string.charCodeAt(1)) // retorna o valor na tabela unicode inicia no 0 
---
+++ fundamentos/templates.js
@@ -0,0 +1,15 @@
+const nome = 'Rebeca';
+const concatenacao = 'Olá ' + nome + '!' 
+
+
+console.log(concatenacao)
+
+const template = `
+
+    olá rebeca!
+
+    ${nome}
+
+`
+
+console.log(template)
