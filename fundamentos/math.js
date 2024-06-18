2bf50139591ab82b84f0fa9dbe0d23d64caed3ef
pedrocabralia <pedrocabralia@hotmail.com>
2024-06-18 12:05:45 -0300

    usando o objeto Math

---
+++ fundamentos/math.js
@@ -0,0 +1,8 @@
+const raio = 5.6
+
+const area = Math.PI * Math.pow(raio,2)
+
+console.log(area) /// resultado 98.5203456165759
+console.log(typeof Math) // object
+
+console.log(area.toFixed(1)) //resultado 98.5
